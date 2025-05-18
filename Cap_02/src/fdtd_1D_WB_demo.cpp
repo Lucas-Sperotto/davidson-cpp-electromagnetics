4252586f5eb8dcaffb70ffc8cf3cd1e4e7eb8e65
Lucas Kriesel Sperotto <sperotto.lucas@outlook.com>
2025-05-18 20:51:52 -0300

    Adicionado comentários ao código.

--- Cap_02/src/fdtd_1D_WB_demo.cpp
+++ Cap_02/src/fdtd_1D_WB_demo.cpp
@@ -19,6 +19,7 @@
 
 #include <string>
 
+// Definindo um alias para a biblioteca filesystem para simplificar chamadas
 namespace fs = std::filesystem;
 
 // Função impulso gaussiano derivado normalizado
@@ -42,10 +43,10 @@ int main()
 
     std::ofstream voltage_log(out_dir + "/WB_voltage_over_time.csv");
 
-    const double C = 1.0;
-    const double L = 1.0;
-    const double Z_0 = std::sqrt(L / C);
-    const double c = 1.0 / std::sqrt(L * C);
+    const double C = 1.0;                    // Capacitância por unidade de comprimento [F/m]
+    const double L = 1.0;                    // Indutância por unidade de comprimento [H/m]
+    const double Z_0 = std::sqrt(L / C);     // Impedância característica da linha de transmissão eq. (2.87)
+    const double c = 1.0 / std::sqrt(L * C); // Velocidade de propagação na linha (v = 1/sqrt(LC))
 
     std::string resp;
     std::cout << "Análise por pulso gaussiano derivado (1) ou função transferência (0)? [1]: ";
@@ -58,8 +59,8 @@ int main()
     // For time-domain demo, use Rs=1, Rl=3 for nice results. Start out with both %=1
     if (flag)
     {
-        h = 0.5;
-        Rs = 1.0;
+        h = 0.5;  // Comprimento total da linha de transmissão (em metros) [m]
+        Rs = 1.0; // Resistência da fonte [Ohm]
         std::cout << "Resistência de carga (default 3 Ohm): ";
         std::getline(std::cin, resp);
         Rl = resp.empty() ? 3.0 : std::stod(resp);
@@ -69,30 +70,30 @@ int main()
     }
     else
     {
-        h = 0.25;
-        Rs = 0.5;
+        h = 0.25; // Comprimento total da linha de transmissão (em metros) [m]
+        Rs = 0.5; // Resistência da fonte [Ohm]
         Rl = 2.0;
         Courant = 0.4;
         sigma = 1.0 / (16.0 * M_PI); // more bandwidth
         Nk = 1024;                   // Number of time steps. Power of two best (for FFT).
     }
 
-    const int Nz = 11; // 41; % 11, 21 and 41 demonstrate effect of dispersion nicely.
-    const double delta_z = h / (Nz - 1);
+    const int Nz = 11;                   // 41; % 11, 21 and 41 demonstrate effect of dispersion nicely.
+    const double delta_z = h / (Nz - 1); // Number of points
     const double delta_t = Courant * delta_z / c;
     const double offset = 4.0 * sigma;
 
-    const double beta1 = 2.0 * delta_t / (Rs * C * delta_z);
-    const double beta2 = 2.0 * delta_t / (Rl * C * delta_z);
+    const double beta1 = 2.0 * delta_t / (Rs * C * delta_z); // Eq. (2.67)
+    const double beta2 = 2.0 * delta_t / (Rl * C * delta_z); // Eq. (2.68)
     const double const_beta = delta_t / (C * delta_z);
-    // const double r = (delta_t)^2/(L*C*delta_z^2)
+    // const double r = (delta_t * delta_t) / (L * C * delta_z * delta_z); // Eq. (2.69)
 
     const double growth = 20.0; // An abritary growth factor indicating instability
     // Don't make too close to 1, since the early time
     // behaviour can indeed grow quite quickly.
 
     std::vector<double> V_nmin1(Nz, 0.0), I_nmin1(Nz, 0.0);
-    std::vector<double> V_n(Nz, 0.0), I_n(Nz, 0.0);
+    std::vector<double> V_n(Nz, 0.0), I_n(Nz, 0.0); // Eq. (2.61) e Eq. (2.62)
     std::vector<double> Vs(Nk, 0.0), Vl(Nk, 0.0), time(Nk, 0.0);
 
     // Cabeçalho: TimeStep, Vn[1], Vn[2], ..., Vn[Nz]
